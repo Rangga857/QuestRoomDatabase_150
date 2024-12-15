c4100926aedbb0ad1fe5122867ecba1d329e06ac
Rangga857 <azharrangga28@gmail.com>
2024-12-15 14:24:55 +0700

    Mendefinisikan MahasiswaDao sebagai Data Access Object di dalam Room. Interface MahasiswaDao juga menyediakan operasi CRUD untuk tabel mahasiswa

---
+++ app/src/main/java/com/example/roomdatabase/data/dao/MahasiswaDao.kt
@@ -0,0 +1,29 @@
+package com.example.roomdatabase.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.roomdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MahasiswaDao {
+
+    @Insert
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    @Query("select * from mahasiswa")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+
+}
