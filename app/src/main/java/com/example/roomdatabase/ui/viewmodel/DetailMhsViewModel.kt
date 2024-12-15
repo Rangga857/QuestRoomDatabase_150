42ffeb2fb5f37350d269b3f65e7b4be600a6bc44
Rangga857 <azharrangga28@gmail.com>
2024-12-15 15:08:31 +0700

    class DetailMhsViewModel digunakan untuk mengelola logika bisnis pada tampilan detail mahasiswa dan berinteraksi dengan repository untuk mengambil dan menghapus data mahasiswa dari database.

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -21,7 +21,44 @@ class DetailMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ) : ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue =  DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMhs(){
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
 
 }
 //data class untuk menampung data yang akan ditampilkan di UI
