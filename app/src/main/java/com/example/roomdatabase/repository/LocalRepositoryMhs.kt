be0326d1456b8b28317615787d3d9a565d1e8d8f
Rangga857 <azharrangga28@gmail.com>
2024-12-15 14:33:11 +0700

    implementasi dari interface RepositoryMhs. Repository bertindak sebagai penghubung antara ViewModel atau Use Case dengan DAO (Data Access Object) di dalam Room. Repostirory ini juga enyediakan metode CRUD untuk tabel mahasiswa.

---
+++ app/src/main/java/com/example/roomdatabase/repository/LocalRepositoryMhs.kt
@@ -0,0 +1,36 @@
+package com.example.roomdatabase.repository
+
+import com.example.roomdatabase.data.dao.MahasiswaDao
+import com.example.roomdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMhs(
+    private val mahasiswaDao: MahasiswaDao) : RepositoryMhs
+{
+    override suspend fun insertMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMahasiswa(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>>{
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim:String) : Flow<Mahasiswa>{
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+
+}
+
