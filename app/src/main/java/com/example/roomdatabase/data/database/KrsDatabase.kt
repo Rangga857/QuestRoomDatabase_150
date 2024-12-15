399801f6ce2d456871128c4a150a8727d27040f6
Rangga857 <azharrangga28@gmail.com>
2024-12-15 14:13:30 +0700

    Deklarasi Kelas KrsDatabase dan  Menentukan Entities dalam database. Di sini hanya ada Mahasiswa::class.

---
+++ app/src/main/java/com/example/roomdatabase/data/database/KrsDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomdatabase.data.dao.MahasiswaDao
+import com.example.roomdatabase.data.entity.Mahasiswa
+
+
+@Database(entities = [Mahasiswa::class], version = 1, exportSchema =  false)
+abstract class KrsDatabase : RoomDatabase() {
+
+}
