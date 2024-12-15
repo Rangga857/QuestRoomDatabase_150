439e926b2aab6136591838b7f1f9c17ea465e29e
Rangga857 <azharrangga28@gmail.com>
2024-12-15 15:12:48 +0700

    menginisialisasi beberapa ViewModel dengan dependensi yang dibutuhkan, terutama untuk MahasiswaViewModel, HomeMhsViewModel, DetailMhsViewModel, dan UpdateMhsViewModel.

---
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.roomdatabase.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase.KrsApp
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer {
+            MahasiswaViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                KrsApp().containerApp.repositoryMhs
+            )
+        }
+
+    }
+}
+
+fun CreationExtras.KrsApp():KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
